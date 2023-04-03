9b8bde1ac4f60326bc00008e2bc12097b48a35c8
= <=>
2023-04-03 17:31:12 -0500

    Adjusted the URL to version 1.2

--- creds.py
+++ creds.py
@@ -1,5 +1,5 @@
 #Credentials for Fitbit API Call
-url = 'https://api.fitbit.com/1/user/'
+url = 'https://api.fitbit.com/1.2/user/'
 access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhYMjIiLCJzdWIiOiI0SjJGM0oiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNzAyOTA4NDI5LCJpYXQiOjE2NzEzNzI0Mjl9.N-mo3LGwSBwpcyDBkNubzkLTM7JCr1BRAlYiA6W0YVQ"
 user_id = "4J2F3J"
 
